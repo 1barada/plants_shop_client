33434bbafa08a37f55f3c89312797dc0b6d74a91
1barada <jionanrouk@gmail.com>
2023-04-24 14:49:59 +0300

    updated server url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const baseServerURL = 'http://localhost:3001';
+export const baseServerURL = 'https://zany-clam-capris.cyclic.app';
 
 export const noImageUrl = 'https://www.meme-arsenal.com/memes/624961d2487835e29025b7df30dfcfab.jpg';
 export const authPageImgUrl = 'https://longwoodgardens.org/sites/default/files/styles/social_share_cropped/public/2023-02/620543_Winter%20Wonder_Orangery_Mathias-Becca.jpg?h=0fb54712&itok=JQsTpSn8';
